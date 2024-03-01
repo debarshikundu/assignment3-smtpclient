bde283fa6e3141896eb2f9bd60f82dc5f25af7c9
Debarshi Kundu <dk3153@nyu.edu>
2024-03-01 18:14:05 -0500

    let/'s see if this works

--- smtpClient.py
+++ smtpClient.py
@@ -5,6 +5,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     msg = "\r\n My message"
     endmsg = "\r\n.\r\n"
 
+    mailserver = ("127.0.0.1", 1025)
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
     clientSocket = socket(AF_INET, SOCK_STREAM)
